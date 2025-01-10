f037e36891e3b4f16809b50e9dd249ab70203f93
pozniej-znajde-wolne-haslo <aniarafeld@o2.pl>
2025-01-10 20:25:35 +0100

    :lipstick: moves main container to App.jsx & corrects the background styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import Container from './components/Container/Container';
+/* import Container from './components/Container/Container';
 
 function App() {
   return (
     <>
       <Container />
     </>
+  );
+}
+
+export default App; */
+
+import './App.scss';
+import Header from './components/header/Header';
+import Hero from './components/Hero/Hero';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="wrapper-content">
+        <Header />
+        <Hero />
+      </div>
+    </div>
   );
 }
 
