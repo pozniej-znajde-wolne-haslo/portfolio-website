a36eb11f8361a233a52a0dd64530007a656abdaa
pozniej-znajde-wolne-haslo <aniarafeld@o2.pl>
2025-01-24 21:13:26 +0100

    chnages email input type to email

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -18,7 +18,7 @@ export default function Contact() {
         </div>
         <div className="form-group">
           <label htmlFor="email">Email</label>
-          <input type="text" name="email" id="email" required />
+          <input type="email " name="email" id="email" required />
         </div>
         <div className="form-group">
           <label htmlFor="message">Your message</label>
